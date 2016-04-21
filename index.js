8848e4d52990b3a1396b868b0a0aede10c3a5195
gagge <tobias@nublar.se>
2016-04-21 11:02:58 +0200

    Put dbmigration back and moved replaceAll-prototype to index.

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
 'use strict';
 
+const dbmigration  = require('larvitdbmigration')({'tableName': 'orders_db_version', 'migrationScriptsPath': __dirname + '/dbmigration'}),
+      events       = require('events'),
+      eventEmitter = new events.EventEmitter(),
+      log          = require('winston');
+
+let dbChecked = false;
+
+dbmigration(function(err) {
+	if (err) {
+		log.error('larvitorder: orders.js: Database error: ' + err.message);
+		return;
+	}
+
+	dbChecked = true;
+	eventEmitter.emit('checked');
+});
+
+String.prototype.replaceAll = function(search, replacement) {
+	let target = this;
+	return target.replace(new RegExp(search, 'g'), replacement);
+};
+
 exports.Order  = require('./order.js');
 exports.Orders = require('./orders.js');
